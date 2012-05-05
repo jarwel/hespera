cbffcf4a84513cae06663949650d25f803ae58be
Jason Wells <jarwel86@gmail.com>
2012-05-05 16:37:36 -0700

    added more stadium geos to baskeball mapper

--- extraction-service/src/main/java/com/hespera/extraction/BasketballMapper.java
+++ extraction-service/src/main/java/com/hespera/extraction/BasketballMapper.java
@@ -4,17 +4,24 @@ import com.hespera.extraction.geo.BasketballGeo;
 
 public enum BasketballMapper {
 
+    BOSTON (BasketballGeo.BOSTON_CELTICS),
 	CHARLOTTE (BasketballGeo.CHARLOTTE_BOBCATS),
 	DALLAS (BasketballGeo.DALLAS_MAVERICKS),
+    DENVER (BasketballGeo.DENVER_NUGGETS),
 	DETROIT (BasketballGeo.DETROIT_PISTONS ),
 	INDIANA (BasketballGeo.INDIANA_PACERS),
+    LA_CLIPPERS (BasketballGeo.LOS_ANGELES_CLIPPERS),
+    LA_LAKERS (BasketballGeo.LOS_ANGELES_LAKERS),
 	MILWAUKEE (BasketballGeo.MILWAUKEE_BUCKS),
 	MEMPHIS (BasketballGeo.MEMPHIS_GRIZZLIES),
 	NEW_JERSEY (BasketballGeo.NEW_JERSEY_NETS),
+    NEW_YORK (BasketballGeo.NEW_YORK_KNICKS),
+    ORLANDO (BasketballGeo.ORLANDO_MAGIC),
 	PHILADELPHIA (BasketballGeo.PHILADELPHIA_76ERS),
 	PORTLAND (BasketballGeo.PORTLAND_TRAIL_BLAZERS),
 	SAN_ANTONIO (BasketballGeo.SAN_ANTONIO_SPURS),
 	TORONTO (BasketballGeo.TORONTO_RAPTORS),
+    UTAH (BasketballGeo.UTAH_JAZZ),
 	WASHINGTON (BasketballGeo.WASHINGTON_WIZARDS);
 		
 	public final BasketballGeo team;
--- extraction-service/src/main/java/com/hespera/extraction/EspnGoComNBA.java
+++ extraction-service/src/main/java/com/hespera/extraction/EspnGoComNBA.java
@@ -20,7 +20,7 @@ public class EspnGoComNBA extends Scraper {
 
 	private static final Pattern tablePattern = Pattern.compile("<table cellpadding=\"3\" cellspacing=\"1\" class=\"tablehead\"><tr class=\"stathead\">(.+?)</table>");
 
-	private static final Pattern entryPattern = Pattern.compile("<td><a href=\"(.+?)\">[a-zA-Z\\. ]+</a> at <a href=\"(.+?)\">[a-zA-Z\\. ]+</a></td><td align=\"right\">(<a href=\"/nba/preview\\?gameId=\\d+\">)?[a-zA-Z0-9 :]+(</a>)?</td>");
+	private static final Pattern entryPattern = Pattern.compile("<td><a href=\"http://espn.go.com/nba/(.+?)\">[a-zA-z ]+</a> at <a href=\"http://espn.go.com/nba/team/(.+?)\">[a-zA-z ]+</a><br>(.+?)<a href=\"(.+?)\" >(.+?)</a></td><td align=\"right\">(<a href=\"(.+?)\">)?\\d{1,2}:\\d{2} [AP]M(</a>)?</td>");
 	private static final Pattern datePattern = Pattern.compile("<td colspan=\"10\">[a-zA-z]+, [a-zA-z]+ \\d{1,2}</td>");
 	private static final Pattern titlePattern = Pattern.compile("<a href=\"(.+?)\">[a-zA-Z\\. ]+</a> at <a href=\"(.+?)\">[a-zA-Z\\. ]+</a>");
 	private static final Pattern timePattern = Pattern.compile("\\d{1,2}:\\d{2} [AP]M");
@@ -34,7 +34,8 @@ public class EspnGoComNBA extends Scraper {
 	
 	    	Matcher tableMatcher = tablePattern.matcher(content);
 	    	while(tableMatcher.find()) {
-	    		String table = content.substring(tableMatcher.start(), tableMatcher.end()); 
+	    		String table = content.substring(tableMatcher.start(), tableMatcher.end());
+
 	    		// Extract date from table header
 	    		String date = null;
 	    		Matcher dateMatcher = datePattern.matcher(table);
@@ -48,12 +49,13 @@ public class EspnGoComNBA extends Scraper {
 	    		Matcher entryMatcher = entryPattern.matcher(table);
 	    		while(entryMatcher.find()) {
 	    			String entry = table.substring(entryMatcher.start(), entryMatcher.end());
-	    			
+                    System.out.println(entry);
 	    			// Extract title from entry
 	    			String title = null;
 	    			Matcher titleMatcher = titlePattern.matcher(entry);
 		      		if(titleMatcher.find()) {
 		      			title = clean(entry.substring(titleMatcher.start(), titleMatcher.end()));
+                        title = title.replace("Tickets| Venue&nbsp;", "");
 	        		}
 		      		
 		      		// Extract time from entry
